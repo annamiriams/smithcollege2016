9d8e721eab52324c510066e4aaa131e377479f8c
annams <annaextrasteckel@gmail.com>
2025-09-25 19:13:40 -0700

    ongoing deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // ✅ use HashRouter
+// import { HashRouter } from 'react-router-dom'; // ✅ use HashRouter for GitHub Pages
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <App />
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>
 );
 // Using HashRouter to ensure routing works correctly on GitHub Pages
+// Using BrowserRouter for local development and other environments like netlify
