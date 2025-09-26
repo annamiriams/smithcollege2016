2d4dc3717d3267bb84b98511e9dfb2a8fd644cf1
annams <annaextrasteckel@gmail.com>
2025-09-25 20:01:51 -0700

    restarting deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { HashRouter } from 'react-router-dom'; // ✅ use HashRouter for GitHub Pages
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
@@ -12,5 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </BrowserRouter>
     </React.StrictMode>
 );
-// Using HashRouter to ensure routing works correctly on GitHub Pages
-// Using BrowserRouter for local development and other environments like netlify
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    // base: '/smithcollege2016/', // MUST match your GitHub repo name exactly
     plugins: [react()],
 });
-// This is needed for GitHub Pages to find your JS and CSS files in the right place.
