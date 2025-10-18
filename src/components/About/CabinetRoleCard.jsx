8665d7a27fd933bf33bad4551beb5f4357772f94
annams <annaextrasteckel@gmail.com>
2025-10-18 16:43:14 -0700

    made role-card images taller to make the overall appearance look nicer

--- src/components/About/CabinetRoleCard.jsx
+++ src/components/About/CabinetRoleCard.jsx
@@ -15,8 +15,7 @@ const CabinetRoleCard = ({
             <img 
                 src={`/${image}.jpg`} 
                 alt={`${name}'s photo`}
-                // Shifting Tia and Kristin's photos down
-                className={`card-image ${name === "Tia Karapoulios" ? "adjust-crop-mobile" : name === "Kristin Rivers" ? "adjust-crop-mobile" : ""}`} 
+                className={`card-image`} 
                 />
             <div className="card-content">
                 <div className="card-header">
@@ -24,8 +23,8 @@ const CabinetRoleCard = ({
                     <span className="pronouns">{pronouns}</span>
                 </div>
                 <div className="card-info">
-                    <p><strong>Position:</strong> {position}</p>
                     <p><strong>House:</strong> {house}</p>
+                    <p><strong>Position:</strong> {house}</p>
                     <p><strong>Major:</strong> {major}</p>
                     <p><strong>Activities:</strong> {activities}</p>
                     <p><strong>Since Graduation:</strong> {sinceGraduation}</p>
