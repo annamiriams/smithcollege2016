0927ce49cdd3da45cbe5893aa6961b22d9d7c7a0
annams <annaextrasteckel@gmail.com>
2025-10-20 19:32:42 -0700

    added lena and celine

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,7 +8,7 @@ const members = [
         pronouns: "she/her",
         position: "Reunion Chair",
         house: "Talbot",
-        major: "Math and Sociology minor",
+        major: "Math & Sociology minor",
         activities: "Smiffenpoofs",
         sinceGraduation:
             "I live in Boston with my husband. I worked in consulting for five years, and now work as a school psychologist in a K-4 school. In my spare time, I love playing soccer and pickleball, reading books, and spending time with friends and family.",
@@ -19,8 +19,8 @@ const members = [
         pronouns: "she/they",
         position: "Reunion Chair",
         house: "Gardiner",
-        major: "Psychology and Jewish Studies",
-        activities: "Smiffenpoofs, SC Jewish Community, Gold Key",
+        major: "Psychology & Jewish Studies",
+        activities: "Smiffenpoofs, SC Jewish Community, Gold Key, House Council",
         sinceGraduation:
             "I live in Portland with my spouse and our two cats. I work in the mental health field and spend my time outside of work birdwatching, reading, and cooking.",
     },
@@ -30,10 +30,21 @@ const members = [
         pronouns: "she/her",
         position: "Reunion Chair",
         house: "Emerson",
-        major: "Music and German Studies minor",
+        major: "Music & German Studies minor",
         activities: "Emerson House VP, Smiffenpoofs",
         sinceGraduation:
             "I live in New York City where I work supporting a research team at a biotech investment company. In my spare time, I take voice lessons, go to the theatre, and read.",
+    },
+    {
+        image: "celine-rosak",
+        name: "Celine Rosak",
+        position: "Souvenir Chair",
+        pronouns: "she/her",
+        house: "Gardiner",
+        major: "Anthropology & French Studies",
+        activities: "House Council",
+        sinceGraduation:
+            "I moved to Seattle, where I discovered the wild world of tech and now work as a marketing director. In my spare time I like picking up new skills—current pursuits include making beeswax taper candles and trying not to cry while skiing. I love spending time over good food with great friends.",
     },
     {
         image: "kristin-rivers",
@@ -45,6 +56,17 @@ const members = [
         activities: "Intervarsity Christian Fellowship, Center for Religious & Spiritual Life reporter & photographer, Boutelle-Day Poetry Center intern, and MANNA Soup Kitchen volunteer",
         sinceGraduation:
             "I ran a personal writing blog, had work published by the Straw Dog Writers Guild and the Young Voices Project, and contributed to contests and showcases with Pen and Muse, Wordhaus, Short Fiction Break, and PowerfulU. I currently work as an editor for two weekly community newspapers and am an advocate for mental health/suicide prevention with the American Foundation for Suicide Prevention MA Chapter. I also became an aunt!",
+    },
+    {
+        image: "lena-wilson",
+        name: "Lena Wilson",
+        position: "Program Chair",
+        pronouns: "she/her",
+        house: "Talbot/Tyler",
+        major: "Film Studies & SWAG",
+        activities: "SEC, Labrys Literary Magazine, Gold Key, theater stuff, haunting the basement of Seelye Hall as an ETS employee",
+        sinceGraduation:
+            "I was a townie for two years working at Oh My, Sweeties, and Amherst Cinema, then I moved to New York City to pursue a career in arts and culture journalism. My work has been featured in The New York Times, The Boston Globe, and The Washington Post, among others. I live in Brooklyn with my wife and our two senior rescue dogs, Neil and Knives.",
     },
     {
         image: "nat-belkov",
@@ -68,34 +90,13 @@ const members = [
         sinceGraduation:
             "I coached Women’s College Basketball at Long Beach State and Saint Mary’s College and now work in Sports Tech at Teamworks.",
     },
-    // {
-    //     image: "lena-wilson",
-    //     name: "Lena Wilson",
-    //     position: "Program Chair",
-    //     pronouns: "",
-    //     house: "",
-    //     major: "",
-    //     activities: "",
-    //     sinceGraduation:
-    //         "",
-    // },
+
     // {
     //     image: "elise-parisian",
     //     name: "Elise Parisian",
     //     position: "Program Chair",
     //     pronouns: "",
     //     house: "",
-    //     major: "",
-    //     activities: "",
-    //     sinceGraduation:
-    //         "",
-    // },
-    // {
-    //     image: "celine-rosak",
-    //     name: "Celine Rosak",
-    //     position: "Souvenir Chair",
-    //     pronouns: "",
-    //     house: "Gardiner",
     //     major: "",
     //     activities: "",
     //     sinceGraduation:
